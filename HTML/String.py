370b1b8d721f7ecf400f0d0b2cf24c6943982794
TomasM <tomasmate@centrum.cz>
2024-12-10 21:01:57 +0100

    Uprava2_Lekce4

---
+++ HTML/String.py
@@ -0,0 +1,16 @@
+# retezec = 'prasopes'
+# print(retezec[5:8])
+
+# myList = [1,2,3,4,"abrakadabra"]
+# print(myList)
+# print(myList[0:2])
+# for _ in myList[: :5]:
+#     print(_)
+
+seznam = [1,2,3,4]
+for _ in seznam:
+    print(_)
+print('\n')
+seznam.append('pes')
+for _ in seznam:
+    print(_)
